165fa959282215096ca528ca11383e56b1d43177
Glitch (humongouschungus) <none>
2019-04-13 20:21:54 +0000

    ☠️💒 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("You are about to enter the lair of chungus. BE CAREFULL"); 
+alert("Keep you hands inside the ride all the time"); 
+
+console.log("You have found the easter egg"); 
+console.log("to look at some hot ugly goats use the link below"); 
+console.log ("https://www.google.com/search?q=hot+ugly+goats&safe=strict&rlz=1C1GCEV_en&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjn7ZCRvsvhAhVIs1QKHQuZBiIQ_AUIDigB&biw=1920&bih=969");   
